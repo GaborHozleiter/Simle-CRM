f16caa11a89c83ed7eef7332e7d9009a0386b37a
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-15 21:59:09 +0200

    added add new user dialog

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,12 +5,23 @@ import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TooltipPosition, MatTooltipModule} from '@angular/material/tooltip';
 import {MatSelectModule} from '@angular/material/select';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogModule
+} from '@angular/material/dialog';
+import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [MatButtonModule, MatIconModule, MatTooltipModule, FormsModule, ReactiveFormsModule,
-     MatSelectModule, MatFormFieldModule
+     MatSelectModule, MatFormFieldModule,
   ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
@@ -18,4 +29,10 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 export class UserComponent {
   positionOptions: TooltipPosition[] = ['above'];
   position = new FormControl(this.positionOptions[0]);
+
+  constructor(private dialog : MatDialog){}
+
+  openDialog(){
+    this.dialog.open(DialogAddUserComponent);
+  }
 }
