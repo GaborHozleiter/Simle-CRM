93055a877bdaee2445f52cb20455f10b9e8a2f0d
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-16 01:00:38 +0200

    added inputfields

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy} from '@angular/core';
 import {
   MAT_DIALOG_DATA,
   MatDialog,
@@ -9,12 +9,18 @@ import {
   MatDialogTitle,
   MatDialogModule
 } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [MatDialogClose, MatDialogActions, MatDialogContent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNativeDateAdapter()],
+  imports: [MatDialogClose, MatDialogActions, MatDialogContent, MatFormFieldModule, MatInputModule, MatDatepickerModule],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
