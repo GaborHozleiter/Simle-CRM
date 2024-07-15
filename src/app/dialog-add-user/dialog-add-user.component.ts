3b40e960ae843e9ac73c7a75beda991ad08280e1
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-15 23:57:10 +0200

    added add new user dialog

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  MatDialogModule
+} from '@angular/material/dialog';
 
 
 @Component({
   selector: 'app-dialog-add-user',
   standalone: true,
-  imports: [],
+  imports: [MatDialogClose, MatDialogActions, MatDialogContent],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss'
 })
 export class DialogAddUserComponent {
 
-  
+  onNoClick(){
+
+  }
+
+  animal(){
+
+  }
 
 }
