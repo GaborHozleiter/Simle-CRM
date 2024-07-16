1a1ae0490ce9c30743ee80cebf1b1a9fbd4de549
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-16 02:18:40 +0200

    connected firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,9 @@ import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideFirebaseApp(() => initializeApp({"projectId":"simple-crm-27cfb","appId":"1:1031503411084:web:52b1a4a5dec4c2f115c8a9","storageBucket":"simple-crm-27cfb.appspot.com","apiKey":"AIzaSyAKGeDjPUHAsyDbl9ratJYQuDcRD8XOM6w","authDomain":"simple-crm-27cfb.firebaseapp.com","messagingSenderId":"1031503411084"})), provideFirestore(() => getFirestore())]
 };
