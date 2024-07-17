824d85a460e2cb66814050ff1268873dce9b920b
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-17 17:24:18 +0200

    added user-detail component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserComponent } from './user/user.component';
-import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserDetailComponent } from '../user-detail/user-detail.component';
 
 export const routes: Routes = [
     {
