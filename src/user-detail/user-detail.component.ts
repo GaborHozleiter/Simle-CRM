5ef9d950f74947e9eae4c210d90072bba0f0b043
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-17 18:27:37 +0200

    read the IDs from the URL

--- src/user-detail/user-detail.component.ts
+++ src/user-detail/user-detail.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-detail',
@@ -8,6 +9,19 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './user-detail.component.html',
   styleUrl: './user-detail.component.scss'
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit{
+
+  userId : any = '';
+
+  constructor(private route : ActivatedRoute){
+
+  }
+
+  ngOnInit(): void {
+      this.route.paramMap.subscribe(paramMap => {
+        this.userId = paramMap.get('id');
+        console.log('Id:' ,this.userId);
+      })
+  }
 
 }
