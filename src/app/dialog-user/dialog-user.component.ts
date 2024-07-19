86ecb86c7b14a121797c1a0c8541a13568b99ccb
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-19 05:23:14 +0200

    added user class

--- src/app/dialog-user/dialog-user.component.ts
+++ src/app/dialog-user/dialog-user.component.ts
@@ -4,11 +4,19 @@ import {
   MatDialogClose,
   MatDialogContent,
 } from '@angular/material/dialog';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-dialog-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatDialogClose],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatInputModule, MatFormFieldModule, FormsModule,
+    MatDatepickerModule
+  ],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-user.component.html',
   styleUrl: './dialog-user.component.scss',
 })
