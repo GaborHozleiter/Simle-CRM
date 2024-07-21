7c45f9ff59e7a2686aa93f4a50b5f8079154b73e
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-21 18:42:13 +0200

    added dialog close function

--- src/app/dialog-user/dialog-user.component.ts
+++ src/app/dialog-user/dialog-user.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { MatDialogActions, MatDialogClose, MatDialogContent } from '@angular/material/dialog';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormControl, FormsModule, NgModel } from '@angular/forms';
@@ -9,11 +9,13 @@ import { User } from '../../models/user.class';
 import { CommonModule } from '@angular/common';
 import { Firestore, collection, addDoc } from '@angular/fire/firestore';
 import { inject } from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-user',
   standalone: true,
-  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatInputModule, MatFormFieldModule, FormsModule, MatDatepickerModule, CommonModule],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatInputModule, MatFormFieldModule, FormsModule, 
+    MatDatepickerModule, CommonModule, MatProgressBarModule],
   providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-user.component.html',
   styleUrls: ['./dialog-user.component.scss'],
@@ -21,7 +23,11 @@ import { inject } from '@angular/core';
 export class DialogUserComponent {
   user = new User();
   birthDate: any;
+  loading = false;
+
   private firestore: Firestore = inject(Firestore);
+
+  constructor(public dialogRef: MatDialogRef<DialogUserComponent>) { }
 
   cencel(): void {
   
@@ -30,11 +36,13 @@ export class DialogUserComponent {
   async saveUser() {
     console.log(this.user);
     this.user.birthDate = this.birthDate.getTime();
-
+    this.loading = true;
     try {
       const usersCollection = collection(this.firestore, 'users');
       await addDoc(usersCollection, { ...this.user });
       console.log('User successfully added!');
+      this.loading = false;
+      this.dialogRef.close();
     } catch (error) {
       console.error('Error adding user: ', error);
     }
