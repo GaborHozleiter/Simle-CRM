a891ae774f1aa18ae9e7b9c4b847ee9a14de82fd
GaborHozleiter <154556447+GaborHozleiter@users.noreply.github.com>
2024-07-22 17:27:03 +0200

    added edit user details dialog

--- src/app/dialog-edit-user/dialog-edit-user.component.ts
+++ src/app/dialog-edit-user/dialog-edit-user.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogActions, MatDialogClose, MatDialogContent, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { User } from '../../models/user.class';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-dialog-edit-user',
   standalone: true,
-  imports: [],
+  imports: [MatDialogContent, MatDialogActions, MatDialogClose, MatInputModule, MatFormFieldModule, FormsModule, 
+    MatDatepickerModule, CommonModule, MatProgressBarModule, MatButtonModule],
+    providers: [provideNativeDateAdapter()],
   templateUrl: './dialog-edit-user.component.html',
   styleUrl: './dialog-edit-user.component.scss'
 })
-export class DialogEditUserComponent {
+export class DialogEditUserComponent implements OnInit{
 
+  user!: User;
+  loading = false;
+  birthDate = Date;
+
+  constructor(public dialogRef: MatDialogRef<DialogEditUserComponent>){
+
+  }
+  ngOnInit(): void {
+      
+  }
+
+  saveUser(){
+
+  }
+
+  cencel(){
+
+  }
 }
